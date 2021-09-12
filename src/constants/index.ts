c41fbcf13a74d3898bb08fb32386efcc9f678e63
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2021-09-12 23:39:48 +0530

    added authorization to useRequest

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://api.themoviedb.org/3/";
+export const BASE_URL = "https://api.themoviedb.org/3";
--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,13 +1,14 @@
 import useSWR from "swr";
 import { BASE_URL } from "../constants";
+import fetcher from "../utils/fetcher";
 
 export const useRequest = (path: string) => {
   if (!path) {
     throw new Error("path is required");
   }
-
-  const url = `${BASE_URL}${path}`;
-  const { data, error } = useSWR(url);
+  const authorization = `?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+  const url = `${BASE_URL}${path}${authorization}`;
+  const { data, error } = useSWR(url, fetcher);
 
   return { data, error };
 };
